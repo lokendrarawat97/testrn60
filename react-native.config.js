fc45fd479f283cd5c6283c54c6b9f1c74ca74bcf
lokendrarawat97 <lokendra.rawat@galaxycard.in>
2019-07-27 14:05:48 +0530

    test app

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    "rn-fetch-blob": {
+      platforms: {
+        android: null // disable Android platform, other platforms will still autolink if provided
+      }
+    }
+  },
+
+  // assets: ["./src/assets/fonts/"]
+};
